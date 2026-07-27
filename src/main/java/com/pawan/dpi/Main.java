d885f559d52fe4b61a7f81bb1708e62b8105cea9
PawanMutanwad <pawanmutanwad@gmail.com>
2026-07-28 02:59:28 +0530

    Initial project foundation

---
+++ src/main/java/com/pawan/dpi/Main.java
@@ -0,0 +1,15 @@
+package com.pawan.dpi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                ============================================
+                   Deep Packet Inspection Engine
+                        Java Edition
+                ============================================
+                """);
+
+    }
+}
